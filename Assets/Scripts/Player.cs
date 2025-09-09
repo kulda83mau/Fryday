a35a46d78faa763035aaf0ee71e36a342ff2dce0
David Žižlavský <zizlavsky_david@seznam.cz>
2025-09-09 23:02:26 +0200

    Local player fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,16 @@ public class Player : NetworkBehaviour
     {
         DontDestroyOnLoad(this);
         transform.position = new Vector3(netId * 2, 0);
-        if (!isLocalPlayer) Destroy(GetComponent<TwoDimensionalAnimationStateController>());
+        if (!isLocalPlayer)
+        {
+            Debug.Log("NOT A LOCAL PLAYER");
+            Destroy(GetComponentInChildren<Camera>());
+            Destroy(GetComponentInChildren<AudioListener>());
+            Destroy(GetComponent<TwoDimensionalAnimationStateController>());
+        } else
+        {
+            Debug.Log("LOCAL PLAYER");
+        }
     }
 
     // Update is called once per frame
